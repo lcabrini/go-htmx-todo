7981c01a9d490943aee43c32e709336dbf512033
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2025-06-05 15:48:52 +0000

    Update schema

--- sql/schema/001_tasks.sql
+++ sql/schema/001_tasks.sql
@@ -1,15 +1,20 @@
 -- +goose Up
 
+create extension if not exists "uuid-ossp";
+
 create type priorities as enum('low', 'medium', 'high');
 
 create table tasks(
     id uuid primary key,
     title text not null,
     description text not null,
-    priority priorities not null default 'medium'
+    priority priorities not null default 'medium',
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
 );
 
 -- +goose Down
 
 drop table tasks;
 drop type priorities;
+drop extension "uuid-ossp";
