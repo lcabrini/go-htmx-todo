5d220afa8ef962a6a8835d2eddc704483d8195e3
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2025-06-05 16:53:31 +0000

    Auto-generate UUID

--- sql/schema/001_tasks.sql
+++ sql/schema/001_tasks.sql
@@ -5,7 +5,7 @@ create extension if not exists "uuid-ossp";
 create type priorities as enum('low', 'medium', 'high');
 
 create table tasks(
-    id uuid primary key,
+    id uuid primary key default uuid_generate_v4(),
     title text not null,
     description text not null,
     priority priorities not null default 'medium',
